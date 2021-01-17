cbeb484e0536303a8c0aa9b7b3d51038affaaddd
AgnikaRouth <agnika.routh@gmail.com>
2021-01-17 19:27:20 +0100

    My first commit

---
+++ app/src/main/java/com/supairvision/savegps/MainActivity.java
@@ -0,0 +1,28 @@
+package com.supairvision.savegps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btMap);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
